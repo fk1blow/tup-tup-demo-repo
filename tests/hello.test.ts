68035721fab2ca7924b8ffff9cec3ce2b1941875
Dragos Tudorache <hedphug@gmail.com>
2026-03-07 21:27:20 +0200

    add tests

---
+++ tests/hello.test.ts
@@ -0,0 +1,11 @@
+import { describe, expect, it } from 'bun:test'
+
+describe('Hello World', () => {
+  it('should pass', () => {
+    expect(true).toBe(true)
+  })
+
+  // it('should fail', () => {
+  //   expect(true).toBe(false)
+  // })
+})
